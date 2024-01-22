c4666bc2533179d8bd1dac31f08c9d92595297e0
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2024-01-23 00:42:55 +0500

    23 Jan/2024 ✅ Getting Articles and Trendings by category is done!

--- app/api/getRelatedArticles/route.ts
+++ app/api/getRelatedArticles/route.ts
@@ -1,5 +1,22 @@
-import { NextResponse } from "next/server";
-const articles_by_category = require("@/database/atricles_by_category.json");
-export async function GET(request: Request) {
-  return NextResponse.json({ message: "GET ARTICLES BY CATEGORY" });
+import { NextResponse, NextRequest } from "next/server";
+const related_articles = require("@/database/atricles_by_category.json");
+export async function GET(req: NextRequest, res: NextResponse) {
+  const category = req.nextUrl.searchParams.get("category");
+  const offset = req.nextUrl.searchParams.get("offset");
+  if (category) {
+    const data = related_articles.article_by_category.filter(
+      (el: any) => el.category === category
+    );
+    if (data.length > 0) {
+      if (offset) {
+        const article = data.slice(~~offset, ~~offset + 10);
+        return NextResponse.json(article);
+      }
+      return NextResponse.json({ error: "Offset is required" });
+    }
+  }
+  return NextResponse.json({
+    error: "category is required",
+    message: "GET TRENDINGS BY CATEGORY",
+  });
 }
--- components/InfiniteScroll.tsx
+++ components/InfiniteScroll.tsx
@@ -1,11 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
-
 import fetchArticles from "@/lib/fetchArticles";
+import fetchRelatedArticles from "@/lib/fetchRelatedArticles";
+import { getRelatedArticles } from "@/redux/slices/articlesSlice";
+import { useAppSelector, AppDispatch } from "@/redux/store";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 
 export default function InfiniteScrollPage() {
-  const [element, setElement] = useState<Article[]>([]);
+  const dispatch = useDispatch<AppDispatch>();
+  const articles: Article[] = useAppSelector((state) => state.articles.value);
+  const category: string | null = useAppSelector(
+    (state) => state.articles.category
+  );
+
+  useEffect(() => {
+    console.log(articles);
+    if (category === null || category === undefined) {
+      console.log("Category undefined ->", category);
+    } else {
+      console.log("Category ->", category);
+    }
+    setOffset(10);
+  }, [category]);
+
   const [offset, setOffset] = useState<number>(0);
   const [pending, setPending] = useState<boolean>(false);
   const elementsContainer = useRef<HTMLUListElement>(null);
@@ -14,8 +33,14 @@ export default function InfiniteScrollPage() {
   useEffect(() => {
     const getInitialArticles = async function () {
       const articles: Article[] = await fetchArticles(0);
-      setElement(articles);
-      // console.log("Initial articles ->", articles);
+      console.log("Initial articles ->", articles);
+
+      dispatch(
+        getRelatedArticles({
+          articles,
+          category: null,
+        })
+      );
       setOffset(10);
     };
     getInitialArticles();
@@ -30,7 +55,8 @@ export default function InfiniteScrollPage() {
             if (entry.isIntersecting) {
               observer!.unobserve(entry.target);
               if (
-                element[element.length - 1].id.toString() === entry.target.id &&
+                articles[articles.length - 1].id.toString() ===
+                  entry.target.id &&
                 offset < 70
               ) {
                 offset < 70 - 10 ? setPending(true) : setPending(false);
@@ -41,13 +67,39 @@ export default function InfiniteScrollPage() {
                   offset
                 );
 
+                if (category === null || category === undefined) {
+                  const articlesData: Promise<Article[]> =
+                    fetchArticles(offset);
+                  articlesData.then((res: Article[]): void => {
+                    dispatch(
+                      getRelatedArticles({
+                        articles: res,
+                        category: null,
+                      })
+                    );
+                    setOffset((prev: number): number => prev + 10);
+                    console.log("Next 10 articles ->", res);
+                  });
+                  console.log("Category undefined ->", category);
+                } else {
+                  console.log("Category ->", category);
+
+                  const articlesData: Promise<Article[]> = fetchRelatedArticles(
+                    category,
+                    offset
+                  );
+                  articlesData.then((res: Article[]): void => {
+                    dispatch(
+                      getRelatedArticles({
+                        articles: res,
+                        category,
+                      })
+                    );
+                    setOffset((prev: number): number => prev + 10);
+                    console.log("Next 10 articles ->", res);
+                  });
+                }
                 // Next 10 articles is fetched here
-                const articlesData: Promise<Article[]> = fetchArticles(offset);
-                articlesData.then((res: Article[]): void => {
-                  setElement((prev: Article[]): Article[] => [...prev, ...res]);
-                  setOffset((prev: number): number => prev + 10);
-                  console.log("Next 10 articles ->", res);
-                });
                 // Next 10 articles is fetched here
               }
             }
@@ -66,51 +118,52 @@ export default function InfiniteScrollPage() {
         observer.disconnect();
       };
     }
-  }, [element, offset]);
+  }, [offset, articles]);
 
   return (
     <div className="flex-1 mx-1">
       <div className="pb-10">
         <ul className="mt-4 flex flex-col gap-2" ref={elementsContainer}>
-          {element.map((el: Article) => (
-            <li
-              id={el.id.toString()}
-              key={el.id}
-              className="flex items-center justify-between p-2 
+          {articles &&
+            articles.map((el: Article) => (
+              <li
+                id={el.id.toString()}
+                key={el.id}
+                className="flex items-center justify-between p-2 
             shadow-lg"
-            >
-              <div className="flex-1 flex flex-col items-start justify-start gap-1">
-                <div className="flex gap-2 items-start justify-start">
-                  <span className="bg-slate-800 text-white px-1 rounded-md">
-                    <strong>#{el.id}</strong>
-                  </span>
-                  <Image
-                    src={"/images/avatar.png"}
-                    alt="avatar"
-                    width={24}
-                    height={24}
-                    className="rounded-full border border-gray-600"
-                  />
-                  <span className="font-bold text-sm">{el.name}</span>
+              >
+                <div className="flex-1 flex flex-col items-start justify-start gap-1">
+                  <div className="flex gap-2 items-start justify-start">
+                    <span className="bg-slate-800 text-white px-1 rounded-md">
+                      <strong>#{el.id}</strong>
+                    </span>
+                    <Image
+                      src={"/images/avatar.png"}
+                      alt="avatar"
+                      width={24}
+                      height={24}
+                      className="rounded-full border border-gray-600"
+                    />
+                    <span className="font-bold text-sm">{el.name}</span>
+                  </div>
+                  <div className="flex flex-col items-start justify-start gap-1">
+                    <h2 className="font-bold text-xl">{el.title}</h2>
+                    <p className="text-sm text-gray-600">{el.description}</p>
+                  </div>
+                  <div className="flex items-center justify-start gap-2">
+                    <span className="text-sm text-gray-500">{el.posted}</span>
+                    &#x2022;
+                    <span className="text-sm text-gray-500">{el.readTime}</span>
+                  </div>
                 </div>
-                <div className="flex flex-col items-start justify-start gap-1">
-                  <h2 className="font-bold text-xl">{el.title}</h2>
-                  <p className="text-sm text-gray-600">{el.description}</p>
-                </div>
-                <div className="flex items-center justify-start gap-2">
-                  <span className="text-sm text-gray-500">{el.posted}</span>
-                  &#x2022;
-                  <span className="text-sm text-gray-500">{el.readTime}</span>
-                </div>
-              </div>
-              <Image
-                alt="flower"
-                src={"/images/white_flower.jpg"}
-                width={160}
-                height={120}
-              ></Image>
-            </li>
-          ))}
+                <Image
+                  alt="flower"
+                  src={"/images/white_flower.jpg"}
+                  width={160}
+                  height={120}
+                ></Image>
+              </li>
+            ))}
         </ul>
       </div>
       {pending && (
