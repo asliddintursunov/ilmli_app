c81b83d7f42e6fdc444d5a8c0780ef4c2de0319f
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2024-01-24 18:04:25 +0500

    24 Jan/2024 ✅ Added Skeleton and scrollTop is fixed

--- components/InfiniteScroll.tsx
+++ components/InfiniteScroll.tsx
@@ -7,6 +7,7 @@ import { useAppSelector, AppDispatch } from "@/redux/store";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
+import Skeleton from "./Skeleton";
 
 export default function InfiniteScrollPage() {
   const dispatch = useDispatch<AppDispatch>();
@@ -122,50 +123,59 @@ export default function InfiniteScrollPage() {
 
   return (
     <div className="flex-1 mx-1">
-      <div className="pb-10">
-        <ul className="mt-4 flex flex-col gap-2" ref={elementsContainer}>
-          {articles &&
-            articles.map((el: Article) => (
-              <li
-                id={el.id.toString()}
-                key={el.id}
-                className="flex items-center justify-between p-2 
+      {articles.length > 0 ? (
+        <div className="pb-10">
+          <ul className="mt-4 flex flex-col gap-2" ref={elementsContainer}>
+            {articles &&
+              articles.map((el: Article) => (
+                <li
+                  id={el.id.toString()}
+                  key={el.id}
+                  className="flex items-center justify-between p-2 
             shadow-lg"
-              >
-                <div className="flex-1 flex flex-col items-start justify-start gap-1">
-                  <div className="flex gap-2 items-start justify-start">
-                    <span className="bg-slate-800 text-white px-1 rounded-md">
-                      <strong>#{el.id}</strong>
-                    </span>
-                    <Image
-                      src={"/images/avatar.png"}
-                      alt="avatar"
-                      width={24}
-                      height={24}
-                      className="rounded-full border border-gray-600"
-                    />
-                    <span className="font-bold text-sm">{el.name}</span>
+                >
+                  <div className="flex-1 flex flex-col items-start justify-start gap-1">
+                    <div className="flex gap-2 items-start justify-start">
+                      <span className="bg-slate-800 text-white px-1 rounded-md">
+                        <strong>#{el.id}</strong>
+                      </span>
+                      <Image
+                        src={"/images/avatar.png"}
+                        alt="avatar"
+                        width={24}
+                        height={24}
+                        className="rounded-full border border-gray-600"
+                      />
+                      <span className="font-bold text-sm">{el.name}</span>
+                    </div>
+                    <div className="flex flex-col items-start justify-start gap-1">
+                      <h2 className="font-bold text-xl">{el.title}</h2>
+                      <p className="text-sm text-gray-600">{el.description}</p>
+                    </div>
+                    <div className="flex items-center justify-start gap-2">
+                      <span className="text-sm text-gray-500">{el.posted}</span>
+                      &#x2022;
+                      <span className="text-sm text-gray-500">
+                        {el.readTime}
+                      </span>
+                    </div>
                   </div>
-                  <div className="flex flex-col items-start justify-start gap-1">
-                    <h2 className="font-bold text-xl">{el.title}</h2>
-                    <p className="text-sm text-gray-600">{el.description}</p>
-                  </div>
-                  <div className="flex items-center justify-start gap-2">
-                    <span className="text-sm text-gray-500">{el.posted}</span>
-                    &#x2022;
-                    <span className="text-sm text-gray-500">{el.readTime}</span>
-                  </div>
-                </div>
-                <Image
-                  alt="flower"
-                  src={"/images/white_flower.jpg"}
-                  width={160}
-                  height={120}
-                ></Image>
-              </li>
-            ))}
-        </ul>
-      </div>
+                  <Image
+                    alt="flower"
+                    src={"/images/white_flower.jpg"}
+                    width={160}
+                    height={120}
+                  ></Image>
+                </li>
+              ))}
+          </ul>
+        </div>
+      ) : (
+        [1, 2, 3, 4, 5, 6].map((i) => {
+          return <Skeleton key={i} image={true} />;
+        })
+      )}
+
       {pending && (
         <div className="grid place-content-center w-full">
           <span className="loading loading-dots loading-lg"></span>
--- components/Trendings.tsx
+++ components/Trendings.tsx
@@ -7,6 +7,7 @@ import { getRelatedTrendings } from "@/redux/slices/trendingsSlice";
 import { AppDispatch, useAppSelector } from "@/redux/store";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import Skeleton from "./Skeleton";
 
 export default function Trendings() {
   const dispatch = useDispatch<AppDispatch>();
@@ -26,35 +27,41 @@ export default function Trendings() {
         <h3 className="text-xl">Trending on Ilmli</h3>
       </div>
       <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:gap-x-10 sm:gap-x-6 gap-y-4">
-        {trendings.map((trending: Article) => {
-          return (
-            <div
-              key={trending.id}
-              className="py-3 px-5 shadow-md hover:shadow-xl flex flex-col justify-between items-start rounded-md cursor-pointer dark:hover:bg-slate-700/20 transition-all"
-            >
-              <div className="flex gap-1 items-start justify-start">
-                <Image
-                  src={"/images/avatar.png"}
-                  alt="avatar"
-                  width={24}
-                  height={24}
-                  className="rounded-full border border-gray-600"
-                />
-                <span className="font-medium text-sm">{trending.name}</span>
-              </div>
-              <div>
-                <span className="text-md font-bold">{trending.title}</span>
-              </div>
-              <div className="flex items-center justify-start gap-2">
-                <span className="text-sm text-gray-500">{trending.posted}</span>
-                &#x2022;
-                <span className="text-sm text-gray-500">
-                  {trending.readTime}
-                </span>
-              </div>
-            </div>
-          );
-        })}
+        {trendings.length > 0
+          ? trendings.map((trending: Article) => {
+              return (
+                <div
+                  key={trending.id}
+                  className="py-3 px-5 shadow-md hover:shadow-xl flex flex-col justify-between items-start rounded-md cursor-pointer dark:hover:bg-slate-700/20 transition-all min-h-[120px] w-full md:w-[255px] lg:w-[320px] xl:w-[425px]"
+                >
+                  <div className="flex gap-1 items-start justify-start">
+                    <Image
+                      src={"/images/avatar.png"}
+                      alt="avatar"
+                      width={24}
+                      height={24}
+                      className="rounded-full border border-gray-600"
+                    />
+                    <span className="font-medium text-sm">{trending.name}</span>
+                  </div>
+                  <div>
+                    <span className="text-md font-bold">{trending.title}</span>
+                  </div>
+                  <div className="flex items-center justify-start gap-2">
+                    <span className="text-sm text-gray-500">
+                      {trending.posted}
+                    </span>
+                    &#x2022;
+                    <span className="text-sm text-gray-500">
+                      {trending.readTime}
+                    </span>
+                  </div>
+                </div>
+              );
+            })
+          : [1, 2, 3, 4, 5, 6].map((i) => {
+              return <Skeleton key={i} image={false} />;
+            })}
       </div>
     </div>
   );
