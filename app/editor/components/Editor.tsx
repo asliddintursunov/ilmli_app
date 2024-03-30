27899972ab5665834b2b286ccadad256dcd271a8
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2024-03-30 21:13:45 +0500

    Sending new post and getting the post is done

--- app/editor/components/Editor.tsx
+++ app/editor/components/Editor.tsx
@@ -8,6 +8,7 @@ import Title from "./Title";
 import Description from "./Description";
 import axios from "axios";
 import { baseURL } from "@/utils";
+import styles from "../style.module.css";
 
 export default function PrimeReactEditor() {
   const [postBody, setPostBody] = useState<string>("");
@@ -19,17 +20,19 @@ export default function PrimeReactEditor() {
 
   const handleSend = function () {
     const postData = {
-      title: postTitle,
-      description: postDescription,
+      title: `<h1>${postTitle}</h1>`,
+      description: `<h2>${postDescription}</h2>`,
       body: postBody,
     };
     axios
       .post(`${baseURL}/create-post`, postData)
-      .then((res) => console.log(res.data))
+      .then((res) => setPost(res.data))
       .catch((err) => console.error(err));
   };
 
   const handleClear = function () {
+    setPostTitle("");
+    setPostDescription("");
     setPostBody("");
   };
 
@@ -44,6 +47,14 @@ export default function PrimeReactEditor() {
       .catch((err) => console.log(err));
   };
 
+  const editorStyle = {
+    height: "50vh",
+    display: postBody && "flex",
+    justifyContent: postBody && "center",
+    cursor: "text",
+    fontSize: "16px",
+  };
+
   return (
     <>
       <div className="flex flex-col gap-2 mb-2">
@@ -56,12 +67,7 @@ export default function PrimeReactEditor() {
       <Editor
         value={postBody}
         onTextChange={(e: any) => setPostBody(e.htmlValue)}
-        style={{
-          height: "50vh",
-          display: postBody && "flex",
-          justifyContent: postBody && "center",
-          cursor: "text",
-        }}
+        style={editorStyle}
         placeholder="Enter text here..."
       />
       <div className="flex items-center gap-2 mt-2">
@@ -77,8 +83,14 @@ export default function PrimeReactEditor() {
         <button className="btn btn-success" onClick={getPost}>
           GET
         </button>
-        <div dangerouslySetInnerHTML={{ __html: post }} />
+        {post && (
+          <div
+            className={styles.postContainer}
+            dangerouslySetInnerHTML={{ __html: post }}
+          />
+        )}
       </div>
+      {/* {postBody} */}
     </>
   );
 }
